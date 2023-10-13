2d021cbfc05df573fbe39047ff6a1a2e49161ae5
cxllmerichie <cxllmerichie@gmail.com>
2023-10-14 00:19:21 +0200

    `nekogram_users` table columns order changed with purpose to get useful info from the foreign key `nekogram_users.id` when using PHPMyAdmin, pgAdmin or other software to handle the administration.

--- NekoGram/storages/mysql/sql.sql
+++ NekoGram/storages/mysql/sql.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `nekogram_users` (
   `id` bigint(20) NOT NULL,
-  `lang` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}',
-  `last_message_id` int(11) DEFAULT NULL,
+  `username` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `full_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `last_message_id` int(11) DEFAULT NULL,
+  `data` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '{}',
+  `lang` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 ALTER TABLE `nekogram_users`
--- NekoGram/storages/pg/tables.sql
+++ NekoGram/storages/pg/tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS "nekogram_users" (
     "id" BIGINT PRIMARY KEY,
-    "lang" VARCHAR(2) NOT NULL DEFAULT 'en',
-    "data" JSONB NOT NULL DEFAULT '{}'::JSONB,
-    "last_message_id" INT DEFAULT NULL,
+    "username" VARCHAR(100) DEFAULT NULL,
     "full_name" VARCHAR(100) NOT NULL,
-    "username" VARCHAR(100) DEFAULT NULL
+    "last_message_id" INT DEFAULT NULL,
+    "data" JSONB NOT NULL DEFAULT '{}'::JSONB,
+    "lang" VARCHAR(2) NOT NULL DEFAULT 'en'
 );
